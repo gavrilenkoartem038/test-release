9fbd58e6d3b3c232d4274aecd8bdc45d3bf20d16
gavirlenkoartem038 <gavrilenkoartem038@gmail.com>
2024-10-17 16:19:15 +0300

    app-header-release

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header-release">
         <img src={logo} className="App-logo-main" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
